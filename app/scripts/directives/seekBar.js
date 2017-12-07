af8fb9be96e9797c2ca048f812eeb3b53c5362ba
warizo16 <wrizo91@gmail.com>
2017-12-07 17:00:01 -0600

    assignment 9

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -33,6 +33,13 @@
              scope.fillStyle = function() {
                  return {width: percentString()};
                 };
+             
+             
+            scope.thumbStyle = function() {
+                return {left: percentString()};
+                 
+                };
+             
              scope.onClickSeekBar = function(event) {
                 var percent = calculatePercent(seekBar, event);
                 scope.value = percent * scope.max;
